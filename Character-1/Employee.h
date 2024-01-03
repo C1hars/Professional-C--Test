d99566517657f5cd403cabf000f24e8aeeacea2b
Chiaki <2829235010@qq.com>
2024-01-03 17:38:21 +0800

    24010202

--- Character-1/Employee.h
+++ Character-1/Employee.h
@@ -8,5 +8,24 @@ namespace Records{
         Employee(const std::string& firstname,const std::string& lastname);
 
         void promote(int raiseAmoout = 1000);
-    };
+        void demote(int demeritAmount = 1000);
+        void hire();
+        void fire();
+        void display() const;
+
+        void setFirstname(const std::string& firstname);
+        const std::string& getFirstname() const;
+
+        void setLastName(const std::string& firstname);
+        const std::string& getLastName() const;
+
+        void setEmployeeNumber(int employeeNumber);
+        int getEmployeeNumber() const;
+
+        void setSalary(int newSalary);
+        int getSalary() const;
+
+        bool isHired() const;
+
+        };
 }
