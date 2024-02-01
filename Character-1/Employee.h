6c7923ee559507c66e18bcad16b559347da5596c
Chiaki <2829235010@qq.com>
2024-02-01 11:25:33 +0800

    240201-Final-sumbit

--- Character-1/Employee.h
+++ Character-1/Employee.h
@@ -1,40 +1,43 @@
 #pragma once
+
 #include <string>
-namespace Records{
-    class Employee
-    {
-        public:
-        Employee() = default;
-        Employee(const std::string& firstname,const std::string& lastname);
 
-        void promote(int raiseAmoout = 1000);
-        void demote(int demeritAmount = 1000);
-        void hire();
-        void fire();
-        void display() const;
+namespace Records {
+	const int kDefaultStartingSalary = 30000;
 
-        void setFirstname(const std::string& firstname);
-        const std::string& getFirstname() const;
+	class Employee
+	{
+	public:
+		Employee() = default;
+		Employee(const std::string& firstName, const std::string& lastName);
 
-        void setLastName(const std::string& firstname);
-        const std::string& getLastName() const;
+		void promote(int raiseAmount = 1000);
+		void demote(int demeritAmount = 1000);
+		void hire(); // Hires or rehires the employee
+		void fire(); // Dismisses the employee
+		void display() const;// Outputs employee info to console
 
-        void setEmployeeNumber(int employeeNumber);
-        int getEmployeeNumber() const;
+		// Getters and setters
+		void setFirstName(const std::string& firstName);
+		const std::string& getFirstName() const;
 
-        void setSalary(int newSalary);
-        int getSalary() const;
+		void setLastName(const std::string& lastName);
+		const std::string& getLastName() const;
 
-        bool isHired() const;
+		void setEmployeeNumber(int employeeNumber);
+		int getEmployeeNumber() const;
 
+		void setSalary(int newSalary);
+		int getSalary() const;
 
+		bool isHired() const;
 
-        private:
-        std::string mFirstName;
-        std::string mLastName;
-        int mEmployeeNumber = -1;
-        int mSalary = 4000;//kDefaultStartingSalary
-        bool mHired = false;
-        
-        };
+	private:
+		std::string mFirstName;
+		std::string mLastName;
+		int mEmployeeNumber = -1;
+		int mSalary = kDefaultStartingSalary;
+		bool mHired = false;
+	};
 }
+
